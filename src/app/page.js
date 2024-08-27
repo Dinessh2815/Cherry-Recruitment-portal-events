3a3dff3e17abb8c4d75e1bb7f5c081eb3c9a900d
Dinessh2815 <dinessh.venkat28@gmail.com>
2024-08-27 20:17:02 +0530

    left section svg

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full min-h-screen overflow-y-auto">
-      {/* Left Section */}
-      <div className="relative flex items-center justify-center w-full md:w-1/2 h-auto md:h-screen">
-        <div
-          className="absolute inset-0 bg-cover bg-center"
-          style={{
-            backgroundImage: "url('/leftevents.png')",
-          }}
+    <div className="flex flex-col md:flex-row w-full min-h-full">
+      {/* Left Section with SVG */}
+      <div className="relative flex items-center justify-center w-full md:w-1/2 h-[70vh] md:h-[100vh] overflow-hidden">
+        {/* SVG Image */}
+        <img
+          src="/eventsleft.svg"
+          alt="Events Left"
+          className="w-full h-full object-cover"
         />
       </div>
 
       {/* Right Section with Full Background */}
-      <div className="relative flex items-center justify-center w-full md:w-1/2 h-auto md:h-screen">
+      <div className="relative flex items-center justify-center w-full md:w-1/2 h-[70vh] md:h-[100vh] overflow-hidden">
         {/* Background Image */}
         <div
           className="absolute inset-0 bg-cover bg-center"

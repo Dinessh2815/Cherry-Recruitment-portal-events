7b8ea3d26ea18ed8eada0d7b46199bf573e2e56b
srivathsan_04 <srivathsan124@gmail.com>
2024-08-28 10:59:20 +0530

    chore: improve page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,58 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
 
 const Page = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full min-h-full">
-      {/* Left Section with SVG */}
-      <div className="relative flex items-center justify-center w-full md:w-1/2 h-[70vh] md:h-[100vh] overflow-hidden">
-        {/* SVG Image */}
-        <img
-          src="/eventsleft.svg"
-          alt="Events Left"
-          className="w-full h-full object-cover"
-        />
-      </div>
-
-      {/* Right Section with Full Background */}
-      <div className="relative flex items-center justify-center w-full md:w-1/2 h-[70vh] md:h-[100vh] overflow-hidden">
-        {/* Background Image */}
+    <div className="flex flex-col h-screen overflow-hidden">
+      <div className="flex flex-1 flex-col md:flex-row">
         <div
-          className="absolute inset-0 bg-cover bg-center"
+          className="relative flex flex-col w-full md:w-1/3 bg-[#FFEF75] rounded-xl overflow-hidden"
           style={{
-            backgroundImage: "url('/eventsbg.png')",
+            border: "15px solid #E8C706",
+            boxSizing: "border-box",
           }}
-        />
+        >
+          <div className="flex-1 flex items-start p-4">
+            <div className="relative w-full h-full">
+              <Image
+                src="/events.png"
+                layout="responsive"
+                width={100}
+                height={100}
+                objectFit="contain"
+                alt="Events"
+              />
+            </div>
+          </div>
+          <div className="flex-1 flex flex-col justify-start p-4 gap-4">
+            <h1 className="text-2xl md:text-4xl font-bold text-black">
+              Brainstorm Blitz
+            </h1>
+            <p className="text-base md:text-lg text-black">Lorem Ipsum</p>
+            <h1 className="text-2xl md:text-4xl font-bold text-black">
+              Crowd Hypnosis
+            </h1>
+            <p className="text-base md:text-lg text-black">
+              Trap the crowd in a blissful trance to aid crowd management
+            </p>
+            <div className="text-base md:text-lg text-black">
+              Orchestrate lorem ipsum lorem ipsum
+            </div>
+          </div>
+        </div>
 
-        {/* Apply Now Button */}
-        <button className="relative z-10 border rounded-full border-white bg-black text-white px-10 md:px-20 py-2 md:py-4">
-          Apply Now
-        </button>
+        <div
+          className="relative flex-1 bg-cover bg-center"
+          style={{ backgroundImage: "url('/eventsbg.png')" }}
+        >
+          <div className="flex items-center justify-center h-full">
+            <button className="relative z-10 border border-white rounded-full bg-black text-white px-8 md:px-16 py-2 md:py-4">
+              Apply Now
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
